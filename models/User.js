6d3e194f9b8eedaa4118bec4246002f880bf9a2f
hussain <hussainalsaffar20@gmail.com>
2024-11-25 00:36:42 +0300

    hash, signin

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const { model, Schema } = require("mongoose");
 
+const PasswordManager = require("../helpers/PasswordManager");
+
 const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
@@ -11,6 +13,13 @@ const UserSchema = new Schema({
       ref: "Recipes",
     },
   ],
+});
+UserSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await PasswordManager.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
 });
 
 module.exports = model("User", UserSchema);
